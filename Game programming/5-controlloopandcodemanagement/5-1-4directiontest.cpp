038fe4f14b25b904d40763461b3c629373dfca97
LYK0520 <2091109044@qq.com>
2022-03-06 21:32:24 +0800

    3/6

---
+++ Game programming/5-controlloopandcodemanagement/5-1-4directiontest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int ch;
+    while(1)
+    {
+        ch=_getch();
+        cout<<" ch="<<ch<<endl;
+    }
+    system("pause");
+    return 0;
+}
