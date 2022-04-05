a423e137ba90ce0ef039d1877ce9fcc792ecfc04
LYK0520 <2091109044@qq.com>
2022-04-05 12:33:48 +0800

    Create 9-1-MineClearance.cpp

---
+++ Game programming/9-MineClearance/9-1-MineClearance.cpp
@@ -0,0 +1,23 @@
+#include<SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+
+
+void Run();
+
+void Initial();
+
+void IniData(); 
+void LoadMediaData();
+void MineSet(int Py,int Px);
+
+void Input();
+void RButtonDown(Vector2i mPoint);
+void LButtonDown(Vector2i mPoint);
+void LButtonDblClk(Vector2i mPoint);
+
+void Logic();
+void isWin();
+void unCover();
+
+void¡¡
